72fdff6497770e7bb2063a86d1e47d0d2cbb4b12
ltzehan <ltzehan13@gmail.com>
2021-12-09 19:07:39 +0800

    Oops

--- tests/cam-capture-vid.sh
+++ tests/cam-capture-vid.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-gst-launch-1.0 rkisp num-buffers=256 device=/dev/video0 io-mode=1 ! \
-  video/x-raw,format=NV12,width=2112,height=1568,framerate=60/1 ! \
+# Note that mpph264enc has max. resolution 1920x1080
+# Expect that the result is stretched due to incompatible aspect ratio 2112x1568 
+# but this suffices for testing
+
+gst-launch-1.0 rkisp num-buffers=128 device=/dev/video0 io-mode=1 ! \
+  video/x-raw,format=NV12,width=1920,height=1080,framerate=60/1 ! \
   mpph264enc ! queue ! h264parse ! mpegtsmux ! \
   filesink location=/home/orangepi/Desktop/test.ts
